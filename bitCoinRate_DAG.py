dc266a26fdff2135b707316bc91d914b54722a72
Konstantin Semenenko <k.emenenko@gmail.com>
2021-12-25 13:58:05 +0300

    --

--- bitCoinRate_DAG.py
+++ bitCoinRate_DAG.py
@@ -19,27 +19,22 @@ def getbitcoinrate(**kwargs):
     if r.status_code == 200:
         data = json.loads(r.text)
 
-        id = data['data']['id']
-        symbol = data['data']['symbol']
-        currencysymbol = data['data']['currencySymbol']
-        rateusd = data['data']['rateUsd']
-        type =  data['data']['type']
+        # id = data['data']['id']
+        # symbol = data['data']['symbol']
+        # currencysymbol = data['data']['currencySymbol']
+        # rateusd = data['data']['rateUsd']
+        # type =  data['data']['type']
 
-        # ti.xcom_push(value=id, key='id')
-        # ti.xcom_push(value=symbol, key='symbol')
-        # ti.xcom_push(value=currencysymbol, key='currencysymbol')
-        # ti.xcom_push(value=rateusd, key='rateusd')
-        # ti.xcom_push(value=type, key='type')
-
-        return (id, symbol, currencysymbol, rateusd, type)
+        #сохраняем в xcom
+        ti.xcom_push(key='id',              value=data['data']['id']),
+        ti.xcom_push(key='symbol',          value=data['data']['symbol']),
+        ti.xcom_push(key='currencysymbol',  value=data['data']['currencysymbol']),
+        ti.xcom_push(key='rateusd',         value=data['data']['rateusd']),
+        ti.xcom_push(key='type',            value=data['data']['type']),
+        
     else:
         print('api response code is: ' + str(r.status_code))
         raise
-
-def print_data(**kwargs):
-    ti = kwargs['ti']
-    print('Полученные значения: {0}'.format(ti.xcom_pull(task_ids='getbitcoinrate')))
-
 
 
 with DAG(
@@ -71,96 +66,19 @@ with DAG(
         dag=dag
     )
 
-    # print_data = PythonOperator(
-    #     task_id='print_data', 
-    #     python_callable=print_data,
-    #     dag=dag
-    # )
-
     
     insert_bitcoinrate = PostgresOperator(
         task_id="insert_bitcoinrate",
         postgres_conn_id="analytics",
         sql="""insert into bitcoinrates2 (id, symbol, currencysymbol, rateusd, type) 
-               values  (%s,%s,%s,%s,%s,)""",
-        parameters = "{{ ti.xcom_pull(task_ids='getbitcoinrate') }}"             
+               values  ('{{ ti.xcom_pull(task_ids='getbitcoinrate', key='id') }}',
+                        '{{ ti.xcom_pull(task_ids='getbitcoinrate', key='symbol') }}', 
+                        '{{ ti.xcom_pull(task_ids='getbitcoinrate', key='currencysymbol') }}',
+                         {{ ti.xcom_pull(task_ids='getbitcoinrate', key='rateusd') }},
+                        '{{ ti.xcom_pull(task_ids='getbitcoinrate', key='type') }}'
+                        )""",
     )
 
-    # insert_bitcoinrate = PostgresOperator(
-    #     task_id="insert_bitcoinrate",
-    #     postgres_conn_id="analytics",
-    #     sql="""insert into bitcoinrates2 (id, symbol, currencysymbol, rateusd, type) 
-    #            values  ('{{ ti.xcom_pull(task_ids='getbitcoinrate', key='id') }}',
-    #                     '{{ ti.xcom_pull(task_ids='getbitcoinrate', key='symbol') }}', 
-    #                     '{{ ti.xcom_pull(task_ids='getbitcoinrate', key='currencysymbol') }}',
-    #                      {{ ti.xcom_pull(task_ids='getbitcoinrate', key='rateusd') }},
-    #                     '{{ ti.xcom_pull(task_ids='getbitcoinrate', key='type') }}'
-    #                     )""",
-    # )
-
-
-    # task_insert_new_row = PostgresOperator(
-    #             task_id='insert_new_row',
-    #             trigger_rule=TriggerRule.ALL_DONE,
-    #             sql='''INSERT INTO table_name VALUES (%s, '{{ ti.xcom_pull(task_ids='execute_bash', key='return_value') }}', %s);''',
-    #             parameters=(uuid.uuid4().int % 123456789, datetime.now()))
 
 
 create_bitcoinrate_table >> getbitcoinrate >> insert_bitcoinrate
-
-
-# def insert_bitcoinRates(id, symbol, currencysymbol, rateusd, type):
-#     conn = None
-#     try:
-#         env = {'filename':'database.ini',
-#                'section':'yandexCloud'
-#         }
-
-#         params = config(**env)
-#         conn = psycopg2.connect(**params)
-#     except (Exception, psycopg2.DatabaseError) as err:
-#         print("Database connection error: {0}".format(err))
-#         raise
-
-#     if conn is not None:
-#         try:
-#             cur = conn.cursor()
-#             cur.execute("insert into bitcoinrates (id, symbol, currencysymbol, rateusd, type) VALUES (%s, %s, %s, %s, %s)", (id, symbol, currencysymbol, rateusd, type))
-#             conn.commit()
-#             cur.close()
-#         except (Exception) as error:
-#             print(error)   
-#             raise
-#         finally:
-#             conn.close()
-
-
-
-
-
-
-
-
-
-# bitCoinRates.doc_md = dedent(
-#         """\
-#     #### Task Documentation
-#     Bla bla bla.
-#     ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-#     """
-#     )
-
-# dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG
-# dag.doc_md = """
-#     This is a documentation placed anywhere
-#     """  # otherwise, type it like this
-# templated_command = dedent(
-#         """
-#     {% for i in range(5) %}
-#         echo "{{ ds }}"
-#         echo "{{ macros.ds_add(ds, 7)}}"
-#         echo "{{ params.my_param }}"
-#     {% endfor %}
-#     """
-# )
-
